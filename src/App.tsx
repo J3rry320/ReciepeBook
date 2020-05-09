26e9b6b5fde4fe4617d3003d49b22696967d08c2
Jerry Satpathy <rutu.satpathy44@gmail.com>
2020-05-10 02:00:47 +0530

    Project Inited with webpack support for tsx and sass

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import "./App.scss";
+export default function App() {
+  return (
+    <div className="welcome">
+      Hey Webpack Works Fine With live Loading and Sass
+    </div>
+  );
+}
